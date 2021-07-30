f9255b14b3870d9649b61ac47345a6fe504fa68f
ich777 <christophhummer@gmail.com>
2021-07-30 13:40:57 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$(grep -E "# set base_path    /var/spool/apt-mirror" ${CONFIG_DIR}/mirror.list)" ]; then
   sed -i "/# set base_path    \/var\/spool\/apt-mirror/c\set base_path    ${MIRROR_DIR}\/apt-mirror" ${CONFIG_DIR}/mirror.list
-  echo "---Please edit your mirror.list file in your conig dir and restart the container when done!---"
+  echo "---Please edit your 'mirror.list' file in your conig directory and restart the container when done!---"
   sleep infinity
 fi
 if [ -z "$(ls -A ${MIRROR_DIR})" ]; then
