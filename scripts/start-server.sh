68d7c449d1e36a7f9c777c8af695f3b69efcbdef
ich777 <christophhummer@gmail.com>
2021-08-05 12:22:27 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "${CRON_SCHEDULE}  /usr/bin/apt-mirror ${CONFIG_DIR}/mirror.list" > ${CONFIG_DIR}/cron
-crontab /tmp/cron
 if [ "$(grep -E "# set base_path    /var/spool/apt-mirror" ${CONFIG_DIR}/mirror.list)" ]; then
   sed -i "/# set base_path    \/var\/spool\/apt-mirror/c\set base_path    ${MIRROR_DIR}" ${CONFIG_DIR}/mirror.list
   echo "---Please edit your 'mirror.list' file in your conig directory and restart the container when done!---"
@@ -14,8 +12,7 @@ fi
 if [ ! -d /var/www/debian ]; then
   ln -s ${MIRROR_DIR}/mirror/$(ls ${MIRROR_DIR}/mirror/)/debian /var/www/debian
 fi
-if [ ! "$(crontab -l 2>/dev/null)" ]; then
- crontab ${CONFIG_DIR}/cron
-fi
+echo "${CRON_SCHEDULE}  /usr/bin/apt-mirror ${CONFIG_DIR}/mirror.list" > ${CONFIG_DIR}/cron
+crontab ${CONFIG_DIR}/cron
 echo "---'apt-mirror' will be run on the following cron schedule: ${CRON_SCHEDULE}---"
 sleep infinity
