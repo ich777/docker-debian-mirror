99d75bb6b6ca88ab0292968528f704aa920021fd
ich777 <christophhummer@gmail.com>
2021-08-05 11:02:19 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -9,6 +9,9 @@ if [ -z "$(ls -A ${MIRROR_DIR})" ]; then
   apt-mirror ${CONFIG_DIR}/mirror.list
   exit 0
 fi
+if [ ! -d /var/www/debian ]; then
+  ln -s ${MIRROR_DIR}/mirror/$(ls ${MIRROR_DIR}/mirror/)/debian /var/www/debian
+fi
 if [ ! "$(crontab -l 2>/dev/null)" ]; then
   echo "${CRON_SCHEDULE}  /usr/bin/apt-mirror ${CONFIG_DIR}/mirror.list" > /tmp/cron
   sleep 1
