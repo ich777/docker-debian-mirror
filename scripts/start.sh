d97863014581b065b09bb756f70e68ae917c8669
ich777 <christophhummer@gmail.com>
2021-08-05 10:03:58 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "---Checking if UID: ${UID} matches user---"
+usermod -u ${UID} ${USER}
+echo "---Checking if GID: ${GID} matches user---"
+usermod -g ${GID} ${USER}
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
